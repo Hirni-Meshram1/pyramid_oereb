397510cbde6e40f16b24c52f591c2a6c039ba452
Hirni-Meshram1 <76470993+Hirni-Meshram1@users.noreply.github.com>
2021-01-08 17:22:49 +0530

    Update 11-init.sql

--- test-db/11-init.sql
+++ test-db/11-init.sql
@@ -1,2 +1,2 @@
-wget https://ubuntu.pkgs.org/18.04/postgresql-amd64/postgresql-10-postgis-2.4_2.4.4+dfsg-4.pgdg18.04+1_amd64.deb.html
-#CREATE EXTENSION IF NOT EXISTS postgis;
+#wget https://ubuntu.pkgs.org/18.04/postgresql-amd64/postgresql-10-postgis-2.4_2.4.4+dfsg-4.pgdg18.04+1_amd64.deb.html
+CREATE EXTENSION IF NOT EXISTS postgis;
